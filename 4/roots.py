4ed2873322dc265c8c032a32350d65a482dd79cd
Neel <neelnarayanshetty@gmail.com>
2023-01-02 19:14:32 +0530

    🔧 fixes

--- 4/roots.py
+++ 4/roots.py
@@ -2,17 +2,18 @@ import math
 
 def check_discriminant(a,b,c):
   discriminant = (b**2) - (4 * a * c)
+  print("the discriminant is - {}".format(discriminant))
   if discriminant == 0:
-    print("discriminants are real and same")
+    print("roots are real and same")
   elif discriminant > 0:
-    print("discriminants are real and different")
+    print("roots are real and different")
   elif discriminant < 0:
-    print("discriminants are complex")
+    print("roots are complex")
   x = (-b-math.sqrt((discriminant)))/(2*a)
   y = (-b+math.sqrt((discriminant)))/(2*a)
   print("The roots are {} and {}".format(x,y))
 
 a=int(input('enter the first number - '))
 b=int(input('enter the second number - '))
-c=int(input('enter the third number -'))
+c=int(input('enter the third number - '))
 check_discriminant(a,b,c)

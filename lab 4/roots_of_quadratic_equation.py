013db471761dc0baac54bf95b7332224e956c4b2
Neel <neelnarayanshetty@gmail.com>
2023-01-07 22:20:22 +0530

    updates

--- lab 4/roots_of_quadratic_equation.py
+++ lab 4/roots_of_quadratic_equation.py
@@ -5,10 +5,12 @@ def calculate_root(a,b,c):
   print("the discriminant is - {}".format(discriminant))
   if discriminant == 0:
     print("roots are real and same")
+    return
   elif discriminant > 0:
     print("roots are real and different")
   elif discriminant < 0:
-    print("roots are complex")
+    print("no real roots")
+    return
   x = (-b-math.sqrt((discriminant)))/(2*a)
   y = (-b+math.sqrt((discriminant)))/(2*a)
   print("The roots are {} and {}".format(x,y))

31eb8980c1e8d03f58a2c5d90b7789534242d16d
Neel <neelnarayanshetty@gmail.com>
2023-02-15 01:02:39 +0530

    updates

--- assignment/1.py
+++ assignment/1.py
@@ -4,12 +4,15 @@ import string
 def gen_password(length):
     password=''
     for i in range(length):
-        password = password + random.choice(string.ascii_lowercase)
+        password += random.choice(string.ascii_lowercase)
     return password
 
 length = int(input('Enter the length of the password (8-16) - '))
 for i in range(5):
   print(gen_password(length if (length>=8 and length<=16) else 10))
+
+
+  
 
 
 
